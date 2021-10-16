ce0e05b4128b68af96916123982a05bb0af7ee87
Aris Zhu Yi Qing <ariszyq123@gmail.com>
2021-10-16 22:16:59 +0100

    IC3RD-3 able to send message to Bitmex and receive message string

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,8 @@
 #include <ixwebsocket/IXWebSocket.h>
 #include <ixwebsocket/IXUserAgent.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main()
 {
@@ -40,39 +42,56 @@ int main()
 
     std::cout << "Connecting to " << url << "..." << std::endl;
 
+    bool established = false;
+
     // Setup a callback to be fired (in a background thread, watch out for race conditions !)
     // when a message or an event (open, close, error) is received
-    webSocket.setOnMessageCallback([](const ix::WebSocketMessagePtr& msg)
-                                   {
-                                       if (msg->type == ix::WebSocketMessageType::Message)
-                                       {
-                                           std::cout << "received message: " << msg->str << std::endl;
-                                           std::cout << "> " << std::flush;
-                                       }
-                                       else if (msg->type == ix::WebSocketMessageType::Open)
-                                       {
-                                           std::cout << "Connection established" << std::endl;
-                                           std::cout << "> " << std::flush;
-                                       }
-                                       else if (msg->type == ix::WebSocketMessageType::Error)
-                                       {
-                                           // Maybe SSL is not configured properly
-                                           std::cout << "Connection error: " << msg->errorInfo.reason << std::endl;
-                                           std::cout << "> " << std::flush;
-                                       }
-                                   }
+    webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg)
+        {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cout << "received message: " << msg->str << std::endl;
+                std::cout << "> " << std::flush;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Open)
+            {
+                established = true;
+                std::cout << "Connection established" << std::endl;
+                std::cout << "> " << std::flush;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                // Maybe SSL is not configured properly
+                std::cout << "Connection error: " << msg->errorInfo.reason << std::endl;
+                std::cout << "> " << std::flush;
+            }
+        }
     );
 
-    // Now that our callback is setup, we can start our background thread and receive messages
+    // Now that our callback is setup,
+    // we can start our background thread and receive messages
     webSocket.start();
-
-    // Send a message to the server (default to TEXT mode)
-    webSocket.send("hello world");
-
     // Display a prompt
     std::cout << "> " << std::flush;
 
+    // sleep for sometime so that message is sent after connection is established
+    int wait_unit_ms = 100;
+    int waited_ms = 0;
+    while (!established)
+    {
+        waited_ms += wait_unit_ms;
+        std::this_thread::sleep_for(std::chrono::milliseconds(wait_unit_ms));
+    }
+    std::cout << "waited " << waited_ms << " milliseconds" << std::endl;
+
+    // send the request
+    webSocket.send("{\"op\":\"subscribe\",\"args\":[\"orderBookL2_25:XBTUSD\",\"instrument\"]}");
+
+
+    // terminate by pressing enter
     std::string text;
+    std::getline(std::cin, text);
+    /*
     // Read text from the console and send messages in text mode.
     // Exit with Ctrl-D on Unix or Ctrl-Z on Windows.
     while (std::getline(std::cin, text))
@@ -80,6 +99,7 @@ int main()
         webSocket.send(text);
         std::cout << "> " << std::flush;
     }
+    */
 
     return 0;
 }
