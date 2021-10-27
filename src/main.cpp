3a34748646ed3a73cb547a59cbc986b62ba4a037
Aris Zhu Yi Qing <ariszyq123@gmail.com>
2021-10-27 17:53:46 +0100

    set up websocket to connect to Binance

--- src/main.cpp
+++ src/main.cpp
@@ -24,13 +24,19 @@ int main()
 
     // Our websocket object
     ix::WebSocket webSocket;
+    ix::WebSocket webSocket2;
 
     // Connect to a server with encryption
     // See https://machinezone.github.io/IXWebSocket/usage/#tls-support-and-configuration
     std::string url("wss://www.bitmex.com/realtime");
     webSocket.setUrl(url);
 
+    // Connect to a server with encryption
+    std::string url2("wss://stream.binance.com:9443/ws/bnbbtc@depth");
+    webSocket2.setUrl(url2);
+
     std::cout << "Connecting to " << url << "..." << std::endl;
+    std::cout << "Connecting to " << url2 << "..." << std::endl;
 
     bool established = false;
 
@@ -85,9 +91,59 @@ int main()
         }
     );
 
+    webSocket2.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg)
+        {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cout << "received message: " << std::endl;
+
+                using json = nlohmann::json;
+                json j = json::parse(msg->str);
+                std::cout << std::setw(4) << j << std::endl;
+
+                /*
+                if (j.contains("data")) {
+                    if (!data) {
+                        data = new MarketData("Bitcoin", j.at("data")[0].at("askPrice"), j.at("data")[0].at("bidPrice"), j.at("data")[0].at("volume"));
+                        data->newMarketData();
+                    } else {
+                        if (j.at("data")[0].contains("askPrice")) {
+                            data->updateSell(j.at("data")[0].at("askPrice"));
+                            data->newMarketData();
+                        }
+                        if (j.at("data")[0].contains("bidPrice")) {
+                            data->updateBuy(j.at("data")[0].at("bidPrice"));
+                            data->newMarketData();
+                        }
+                        if (j.at("data")[0].contains("volume")) {
+                            data->updateVolume(j.at("data")[0].at("volume"));
+                            data->newMarketData();
+                        }
+                    }
+                }
+                */
+
+                std::cout << "> " << std::flush;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Open)
+            {
+                established = true;
+                std::cout << "Connection established" << std::endl;
+                std::cout << "> " << std::flush;
+            }
+            else if (msg->type == ix::WebSocketMessageType::Error)
+            {
+                // Maybe SSL is not configured properly
+                std::cout << "Connection error: " << msg->errorInfo.reason << std::endl;
+                std::cout << "> " << std::flush;
+            }
+        }
+    );
+
     // Now that our callback is setup,
     // we can start our background thread and receive messages
     webSocket.start();
+    webSocket2.start();
     // Display a prompt
     std::cout << "> " << std::flush;
 
@@ -104,6 +160,7 @@ int main()
     // send the request
     webSocket.send("{\"op\":\"subscribe\",\"args\":[\"instrument:XBTUSD\"]}");
 
+    // webSocket2.send("{\"method\":\"SUBSCRIBE\",\"params\":[\"btcusd@depth\"],\"id\":1}");
 
     // terminate by pressing enter
     std::string text;
