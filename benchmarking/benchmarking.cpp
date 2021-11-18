5d96cc960c8642e5ed9c7646d902fed33a4d9d74
Panos <pp419@ic.ac.uk>
2021-11-18 19:57:30 +0000

    Implemented benchmarking for some functions

--- benchmarking/benchmarking.cpp
+++ benchmarking/benchmarking.cpp
@@ -1,20 +1,79 @@
 //#include <../include/benchmark/include/benchmark/benchmark.h>
 
 #include <benchmark/benchmark.h>
+#include "dataManager/OrderDataStore.h"
+#include "bollingerBand/BollingerBand.h"
+#include "exchange/OrderTypes.h"
+#include "exchange/Exchange.h"
+#include "exchange/OrderData.h"
+#include "exchange/TradingStrategy.h"
 
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state)
-        std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
 
 // Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
-    for (auto _ : state)
-        std::string copy(x);
+static void BM_OrderBook_getHighestBid(benchmark::State &state) {
+    OrderBook book{Exchange::BITMEX};
+    for (auto _: state) {
+        book.getHighestBid();
+    }
 }
-BENCHMARK(BM_StringCopy);
+
+BENCHMARK(BM_OrderBook_getHighestBid);
+
+// Define another benchmark
+static void BM_OrderBook_getLowestAsk(benchmark::State &state) {
+    OrderBook book{Exchange::BITMEX};
+    for (auto _: state) {
+        book.getLowestAsk();
+    }
+}
+
+BENCHMARK(BM_OrderBook_getLowestAsk);
+
+// Define another benchmark
+static void BM_OrderDataStore_addEntry(benchmark::State &state) {
+    // OrderTypes::OrderType type, Exchange::ExchangeName name, long price,
+    //            double volume
+//    OrderData test_data {OrderTypes::ASK, Exchange::BITMEX, 500, 100};
+    std::shared_ptr ptr = make_shared<OrderData>(OrderTypes::ASK, Exchange::BITMEX, 500, 100);
+    OrderDataStore dataStore{};
+    for (auto _: state) {
+        dataStore.addEntry(ptr);
+
+    }
+}
+
+BENCHMARK(BM_OrderDataStore_addEntry);
+
+// OrderDataStore but path where key already exists...but volume != 0
+static void BM_OrderDataStore_addEntry_keyAlreadyExists(benchmark::State &state) {
+    std::shared_ptr ptr = make_shared<OrderData>(OrderTypes::ASK, Exchange::BITMEX, 500, 100);
+    OrderDataStore dataStore{};
+   dataStore.addEntry(ptr);
+   for (auto _: state) {
+        dataStore.addEntry(ptr);
+    }
+}
+
+BENCHMARK(BM_OrderDataStore_addEntry_keyAlreadyExists);
+
+static void BM_OrderDataStore_addEntry_keyAlreadyExists_zeroVolume(benchmark::State &state) {
+    std::shared_ptr ptr = make_shared<OrderData>(OrderTypes::ASK, Exchange::BITMEX, 0, 0);
+    OrderDataStore dataStore{};
+    dataStore.addEntry(ptr);
+    for (auto _: state) {
+        dataStore.addEntry(ptr);
+    }
+}
+
+BENCHMARK(BM_OrderDataStore_addEntry_keyAlreadyExists_zeroVolume);
+
+// Define another benchmark
+static void BM_BollingerBand_runStrat(benchmark::State &state) {
+    BollingerBand band{};
+    for (auto _: state)
+        band.runStrategy();
+}
+
+BENCHMARK(BM_BollingerBand_runStrat);
 
 BENCHMARK_MAIN();
