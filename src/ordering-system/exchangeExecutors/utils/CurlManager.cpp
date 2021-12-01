0bdae59bc879b6c9528a6833f86d8e13474d9894
crjake <crjake@gmail.com>
2021-12-01 21:58:41 +0000

    Added preprocessor directives to disable curl functionality upon compiling with ENABLE_CPP_BENCHMARKS flag.

--- src/ordering-system/exchangeExecutors/utils/CurlManager.cpp
+++ src/ordering-system/exchangeExecutors/utils/CurlManager.cpp
@@ -13,19 +13,26 @@ size_t write_data(void* buffer, size_t size, size_t nmemb, void* userp) {
 }
 
 void CurlManager::initCurl() {
+#ifndef ENABLE_CPP_BENCHMARKS
   this->curl = curl_easy_init();
   this->chunk = nullptr;
+#endif
 }
 
 void CurlManager::appendHeader(const std::string& header) {
+#ifndef ENABLE_CPP_BENCHMARKS
   this->chunk = curl_slist_append(chunk, header.c_str());
+#endif
 }
 
 void CurlManager::appendHeadersToRequest() {
+#ifndef ENABLE_CPP_BENCHMARKS
   curl_easy_setopt(this->curl, CURLOPT_HTTPHEADER, this->chunk);
+#endif
 }
 
 void CurlManager::sendRequest(bool output) {
+#ifndef ENABLE_CPP_BENCHMARKS
   if (!output) {
     curl_easy_setopt(this->curl, CURLOPT_WRITEFUNCTION, write_data);
   }
@@ -40,12 +47,17 @@ void CurlManager::sendRequest(bool output) {
       std::cout << "\n";
     }
   }
+#endif
 }
 void CurlManager::addDestination(const std::string& destination) {
+#ifndef ENABLE_CPP_BENCHMARKS
   curl_easy_setopt(this->curl, CURLOPT_URL, destination.c_str());
+#endif
 }
 void CurlManager::addPostFields(const std::string& fields) {
+#ifndef ENABLE_CPP_BENCHMARKS
   curl_easy_setopt(this->curl, CURLOPT_POST, 1L);
   curl_easy_setopt(this->curl, CURLOPT_POSTFIELDS, fields.c_str());
   curl_easy_setopt(this->curl, CURLOPT_POSTFIELDSIZE, fields.length());
+#endif
 }
