c1caad80c4a82b82cc74b722bb78d5f57acb94c7
Kamil Bujel <kamilbujel@gmail.com>
2021-12-03 14:08:34 +0100

    perf flamegraph done

--- benchmark-perf.sh
+++ benchmark-perf.sh
@@ -7,7 +7,9 @@ trap int_trap INT
 
 echo "Running perf, please press CTRL-C to finish the benchmark."
 
-perf record -e cpu-clock --call-graph fp -F 10000 --delay 10000 ./bin/main > /dev/null 2>&1
-perf report -g fractal --stdio --hide-unresolved --dsos=main > $1
+perf record -e cpu-clock --user-callchains -g -F 10000 --delay 10000 ./bin/main > /dev/null 2>&1
+
+perf report --stdio --no-children -n -g folded,0,caller,count --dsos main -s dso --ignore-callees Listener::collectOrderData --hide-unresolved | awk '/^ / { comm = $3 } /^[0-9]/ {cnt=$1; $1=""; print comm ";" $0, cnt}' | ./lib/FlameGraph/flamegraph.pl perf_folded.out > $1
+
 echo "DONE"
-echo "Output written to " $1
+echo "FlameGraph written to" $1
