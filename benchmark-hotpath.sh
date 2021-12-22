b8851a9601a341a6bb1e4b3dedc83b2718765274
crjake <crjake@gmail.com>
2021-12-22 11:57:24 +0400

    Added benchmarking scripts without call to automated test generation.

---
+++ benchmark-hotpath.sh
@@ -0,0 +1,7 @@
+echo "Running the benchmark on the hotpath."
+cmake -S . -DBENCHMARK_HOTPATH:BOOL=True -DENABLE_CPP_BENCHMARKS:BOOL=False\
+-B build
+cmake --build build
+cmake --install build
+./bin/benchmarker
+echo "DONE"
